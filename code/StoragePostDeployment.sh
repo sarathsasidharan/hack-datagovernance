dddf1208788d43f93942d143e06af150bd5d0a02
Sarath Sasidharan <sasasid@microsoft.com>
2021-10-15 12:30:46 +0200

    Added new File for Storage IAM assingments

---
+++ code/StoragePostDeployment.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo " All post deployment role allocations happen here , for the main trigger refer to main.bicep under infra"
--- code/SynapsePostDeployment.sh
+++ code/SynapsePostDeployment.sh
@@ -2,7 +2,7 @@
 
 synapseContributorsObjId=$1
 
-echo " Post Deployment Setup for Azure Synapse Goes in here"
+echo " Post Deployment Setup for Azure Synapse Goes in here ,for the main trigger refer to main.bicep under infra"
 
 # Set the Group of Synapse Contributors 
 az synapse role assignment create --workspace-name hammerah01 \
